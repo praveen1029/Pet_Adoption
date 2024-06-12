0111913537e3ddb4e0a4061870d7d137ba6df032
Praveen <praveenpradeepkumar44@gmail.com>
2024-06-12 17:05:32 +0530

    list update

--- PET ADOPTION/frontend/src/components/pet_list.jsx
+++ PET ADOPTION/frontend/src/components/pet_list.jsx
@@ -1,13 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { Navbar, Nav, Container } from 'react-bootstrap';
 import BaseURL from "./base_url";
-import logo from "../images/pet_logo.png";
-import '../components/pet_list.css'
 import { MyNavbar } from './landing';
+import '../components/pet_list.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from "react-router-dom";
 
-const PetList = () =>{
+export const PetCards = () => {
     const [pets, setPets] = useState([]);
 
     useEffect(() => {
@@ -23,23 +22,32 @@ const PetList = () =>{
 
     return (
         <>
+            <div className="pet-card-div mt-4">
+                {pets.map(pet => (
+                    <div className="pet-card" key={pet.id}>
+                        <div className="pet-img mt-2">
+                            <img src={`${pet.image}`} alt={pet.category} width="285" />
+                        </div>
+                        <div className="pet-content-div mt-4 ms-2 me-2 mb-4">
+                            <h5>{pet.category}</h5> 
+                            <Link to="/user_page"><button className="btn btn-danger view-btn"><i className="fa fa-eye"></i>&nbsp;View</button></Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export const PetList = () => {
+    return (
+        <>
             <MyNavbar hideHome={true} hideLogin={true} hideAdoptions={true} hideProfile={true} />
             <div className='container-fluid pet-list-div'>
                 <div className="row">
-                <div className='pet-div'>
-                <ul>
-                        {pets.map(pet => (
-                            <li key={pet.id}>
-                                <h2>{pet.category}</h2>
-                                <p>{pet.description}</p>
-                                <p>{pet.remark}</p>
-                                <img src={`${pet.image}`} alt={pet.category} width="200" />
-                            </li>
-                        ))}
-                    </ul>
+                    <div className='pet-div'>
+                        <PetCards />
                     </div>
-
-
                 </div>
             </div>
         </>
